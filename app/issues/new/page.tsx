f5bba13d0a9edf0d3a9d3b85bd25f9fc065f0269
lukman bolaji <bolajiakanji21@gmail.com>
2024-08-18 05:32:56 -0700

    Refactor- move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -25,7 +25,18 @@ const NewIssuePage = () => {
   } = useForm<IssueForm>({
     resolver: zodResolver(createIssueSchema),
   });
-  const [error, setError] = useState("");
+    const [error, setError] = useState("");
+    
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+          setIsSubmitting(true);
+          await axios.post("/api/issues", data);
+          router.push("/issues");
+        } catch (error) {
+          setError("unexpected error occured");
+          setIsSubmitting(false);
+        }
+      })
 
   return (
     <div className="max-w-xl ">
@@ -35,17 +46,8 @@ const NewIssuePage = () => {
         </Callout.Root>
       )}
       <form
-        className="space-y-3 "
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setError("unexpected error occured");
-            setIsSubmitting(false);
-          }
-        })}
+              className="space-y-3 "
+              onSubmit={onSubmit}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
