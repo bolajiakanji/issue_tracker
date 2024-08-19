7ceadd9e5e17b834526728bdc4cfd8a2ce040a44
lukman bolaji <bolajiakanji21@gmail.com>
2024-08-19 00:51:06 -0700

    Show issue details

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,20 +3,24 @@ import { notFound } from "next/navigation";
 import React from "react";
 
 interface Props {
-    params: { id: string }
+  params: { id: string };
 }
 
 const IssueDetailPage = async ({ params: { id } }: Props) => {
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(id)}
-    })
-    if (!issue) notFound()
-    return (<div> 
-        <p>{issue.title}</p>
-        <p>{issue.description}</p>
-        <p>{issue.status}</p>
-        <p>{issue.createdAt.toDateString()}</p>
-  </div>);
+  if ( isNaN(parseInt(id))) notFound();
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (!issue) notFound();
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.status}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  );
 };
 
 export default IssueDetailPage;
