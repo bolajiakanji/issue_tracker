db07c8921cd1ffd53f0e3638793a135274f8805c
lukman bolaji <bolajiakanji21@gmail.com>
2024-08-20 00:38:20 -0700

    Add aditional loading skeleton

--- app/components/IssueStatusBadge.tsx
+++ app/components/IssueStatusBadge.tsx
@@ -1,20 +1,18 @@
-import { Status } from "@prisma/client";
-import { Badge } from "@radix-ui/themes";
-import React from "react";
+import { Status } from '@prisma/client'
+import { Badge } from '@radix-ui/themes'
+import React from 'react'
 
-const statusMap: Record<
-  Status,
-  { label: string; color: "violet" | "green" | "red" }
-> = {
-  OPEN: { label: "Open", color: "red" },
-  CLOSED: { label: "Close", color: "green" },
-  PROGRESS: { label: "In Progree", color: "violet" },
-};
+const statusMap: Record<Status, { label: string, color: 'violet' | 'green' | 'red' }> = {
+    OPEN: { label: 'Open', color: 'red' },
+    CLOSED: { label: 'Close', color: 'green' },
+    PROGRESS: { label: 'In Progree', color: 'violet' },
+    
+}
 
-const IssueStatusBadge = ({ status }: { status: Status }) => {
+const IssueStatusBadge = ({ status }: { status: Status } ) => {
   return (
     <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
-  );
-};
+  )
+}
 
-export default IssueStatusBadge;
+export default IssueStatusBadge
---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,24 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Heading, Flex, Card } from "@radix-ui/themes";
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const LoadingIssueDetailPage = () => {
+  return (
+    <div>
+      <Heading>
+        <Skeleton />
+      </Heading>
+      <Flex className="space-x-3" my="2">
+        <Skeleton width='5rem' />
+        <Skeleton width='8rem'  />
+      </Flex>
+      <Card className="prose mt-4">
+        <Skeleton count={3}/>
+      </Card>
+    </div>
+  );
+};
+
+export default LoadingIssueDetailPage;
