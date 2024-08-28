6be5fc4a2bb67fa88a7f9d731f4f74953ed15c4b
lukman bolaji <bolajiakanji21@gmail.com>
2024-08-28 14:54:53 -0700

    Add api for asssigning user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -5,16 +5,13 @@ import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(
-
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-    if (!session)
-    return NextResponse.json({}, { status: 401 })
+  if (!session) return NextResponse.json({}, { status: 401 });
 
-  
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
@@ -42,10 +39,9 @@ export async function DELETE(
   request: NextRequest,
   { params: { id } }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-    if (!session)
-    return NextResponse.json({}, { status: 401 })
+  if (!session) return NextResponse.json({}, { status: 401 });
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
